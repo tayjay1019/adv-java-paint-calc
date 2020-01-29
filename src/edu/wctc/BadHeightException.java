d69a5218950f609e84a36ed18a2a6269f8867aa8
Stacy Read <stacy.read.007@gmail.com>
2020-01-29 01:28:51 -0600

    Initial commit

---
+++ src/edu/wctc/BadHeightException.java
@@ -0,0 +1,7 @@
+package edu.wctc;
+
+public class BadHeightException extends Exception {
+    public BadHeightException(String message) {
+        super(message);
+    }
+}
---
+++ src/edu/wctc/BadWidthException.java
@@ -0,0 +1,7 @@
+package edu.wctc;
+
+public class BadWidthException extends Exception {
+    public BadWidthException(String message) {
+        super(message);
+    }
+}
